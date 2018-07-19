cf8b26ff4ad9a27054153e51888023bd6977561c
@snailsack <nickrailsback@gmail.com>
2018-07-19 16:54:20 -0400

    set up server, installed mime-types

---
+++ chat_server.js
@@ -0,0 +1,41 @@
+// SETUP REQUIREMENTS
+const http = require('http');
+const fs = require ('fs');
+const mime = require('mime-types');
+const port = process.env.PORT || 5000;
+
+http.createServer(handleRequest).listen(port);
+console.log("Listening on port: " + port);
+
+function handleRequest(request, response) {
+
+
+ 
+    let fileName;
+    let data;
+    let type = 'text/plain';
+    try {
+        if (request.url === '/') {
+            fileName = 'index.html'
+        } else {
+            fileName = request.url.slice(1)
+        }
+         data = fs.readFileSync(fileName);
+         type = mime.lookup(fileName);
+         response.statusCode = 200;
+    } catch (error) {
+        console.log(error);
+        data = "Error: " + error.toString();
+        response.statusCode = 404;
+    }
+    
+
+    let contentType = `${type}; charset=utf-8`
+    
+    response.setHeader('Content-Type', contentType);
+
+    response.write(data);
+
+    response.end();
+}
+
