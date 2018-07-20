2913b4cbb4d97e0c5770e47b87e4d769e21046b6
@snailsack <nickrailsback@gmail.com>
2018-07-20 15:58:19 -0400

    added assistant class/file

--- chat_server.js
+++ chat_server.js
@@ -2,18 +2,21 @@
 const http = require('http');
 const fs = require ('fs');
 const mime = require('mime-types');
+const urlParser = require('url');
 const port = process.env.PORT || 5000;
 
 http.createServer(handleRequest).listen(port);
 console.log("Listening on port: " + port);
 
 function handleRequest(request, response) {
+    console.log("request.url = " + request.url);
 
-
- 
+    let url = urlParser.parse(request.url);
+    let path = url.pathname;
     let fileName;
     let data;
     let type = 'text/plain';
+    
     try {
         if (request.url === '/') {
             fileName = 'index.html'
